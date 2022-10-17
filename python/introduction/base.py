e3c6482d16b63f730352122793c0c0ce06bb7d44
Bernardo Dias <bernardo.araujo@aluno.ufop.edu.br>
2022-10-17 10:13:29 -0300

    arquivo base criado para treinar python

---
+++ python/introduction/base.py
@@ -0,0 +1,12 @@
+#Uma introdução básica aos comandos de python
+print('Este é um arquivo para treino.\n')
+
+name = input('My name is ')
+age = int(input('and i have '))
+
+print(10 + age)
+print(name + 'Lucas')
+print(name * age)
+
+print(age ** 2) 
+#print(age^2) Potência não funciona assim
